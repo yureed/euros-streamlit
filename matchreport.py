02fa93fd899667bf6a61b74c055c82bbef83aeeb
yureed <41127970+yureed@users.noreply.github.com>
2024-06-18 20:25:53 +0500

    Update matchreport.py

--- matchreport.py
+++ matchreport.py
@@ -34,8 +34,8 @@ from supabase import create_client, Client
 # Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
+    url = st.secrets["url"]
+    key = st.secrets["key"]
     return create_client(url, key)
 
 supabase = init_connection()
